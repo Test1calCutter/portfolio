12341080aa9ba8af9fec3259f55bf39b058d5290
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-05-25 10:25:11 -0700

    Add files via upload

---
+++ links.js
@@ -0,0 +1,39 @@
+let contact = document.getElementById("contact")
+let projects = document.getElementById("projects")
+let home = document.getElementById("home")
+let portfolio = document.getElementById("portfolio")
+
+function contacts(){
+    window.location.assign("links/contact/contact.html")
+}
+
+function projectss(){
+    window.location.assign("links/projects/projects.html")
+}
+
+function contacts(){
+    window.location.assign("links/contact/contact.html")
+}
+
+function portfolios() {
+    window.location.assign("links/portfolio/portfolio.html")
+}
+
+
+//Links
+
+function homes() {
+    window.location.assign("../../index.html")
+}
+
+function lprojectss(){
+    window.location.assign("../projects/projects.html")
+}
+
+function lcontacts(){
+    window.location.assign("../contact/contact.html")
+}
+
+function lportfolios() {
+    window.location.assign("../portfolio/portfolio.html")
+}
